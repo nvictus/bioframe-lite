d1456c5d05025ba8ec4ced32a48ef5e51573ebe7
Nezar Abdennur <nabdennur@gmail.com>
2023-07-21 08:27:50 -0400

    Fix tests

--- tests/test_joinops.py
+++ tests/test_joinops.py
@@ -5,8 +5,8 @@ from bioframe_lite._ops import (
     aranges_flat,
     overlap_self,
     overlap,
-    closest_nooverlap_self,
-    closest_nooverlap
+    closest_self,
+    closest
 )
 
 def test_aranges_flat():
@@ -42,17 +42,17 @@ def test_closest_nooverlap_self():
     starts = np.array([1, 3, 4, 6])
     ends =   np.array([2, 5, 7, 6])
 
-    ev1, ev2, dists = closest_nooverlap_self(starts, ends, k=1)
+    ev1, ev2, dists = closest_self(starts, ends, include_overlaps=False, k=1)
     assert_array_equal(ev1, np.array([0, 1, 2, 3]))
     assert_array_equal(ev2, np.array([1, 0, 0, 3]))
     assert_array_equal(dists, np.array([1, 1, 2, 0]))
 
-    ev1, ev2, dists = closest_nooverlap_self(starts, ends, k=1, direction="left")
+    ev1, ev2, dists = closest_self(starts, ends, include_overlaps=False, k=1, direction="left")
     assert_array_equal(ev1, np.array([1, 2, 3]))
     assert_array_equal(ev2, np.array([0, 0, 3]))
     assert_array_equal(dists, np.array([1, 2, 0]))
 
-    ev1, ev2, dists = closest_nooverlap_self(starts, ends, k=1, direction="right")
+    ev1, ev2, dists = closest_self(starts, ends, include_overlaps=False, k=1, direction="right")
     assert_array_equal(ev1, np.array([0, 1, 3]))
     assert_array_equal(ev2, np.array([1, 3, 3]))
     assert_array_equal(dists, np.array([1, 1, 0]))
@@ -64,17 +64,17 @@ def test_closest_nooverlap():
     starts2 = np.array([1, 2, 4, 6])
     ends2 =   np.array([2, 5, 7, 6])
 
-    ev1, ev2, dists = closest_nooverlap(starts1, ends1, starts2, ends2, k=1)
+    ev1, ev2, dists = closest(starts1, ends1, starts2, ends2, include_overlaps=False, k=1)
     assert_array_equal(ev1, np.array([0, 1, 2, 3]))
     assert_array_equal(ev2, np.array([1, 0, 0, 3]))
     assert_array_equal(dists, np.array([0, 1, 2, 0]))
 
-    ev1, ev2, dists = closest_nooverlap(starts1, ends1, starts2, ends2, k=1, direction="left")
+    ev1, ev2, dists = closest(starts1, ends1, starts2, ends2, include_overlaps=False, k=1, direction="left")
     assert_array_equal(ev1, np.array([1, 2, 3]))
     assert_array_equal(ev2, np.array([0, 0, 3]))
     assert_array_equal(dists, np.array([1, 2, 0]))
 
-    ev1, ev2, dists = closest_nooverlap(starts1, ends1, starts2, ends2, k=1, direction="right")
+    ev1, ev2, dists = closest(starts1, ends1, starts2, ends2, include_overlaps=False, k=1, direction="right")
     assert_array_equal(ev1, np.array([0, 1, 3]))
     assert_array_equal(ev2, np.array([1, 3, 3]))
     assert_array_equal(dists, np.array([0, 1, 0]))
